3998035b67e0a0dd709f65cbc14ee8e72cbb1327
Ccelestine-0 <81595656+Ccelestine-0@users.noreply.github.com>
2022-03-30 14:55:23 +1100

    Add files via upload

---
+++ Computing sac task 3/Computing sac task 3/Sale.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Computing_sac_task_3
+{
+    internal class Sale
+    {
+        //grabbing the columns from the csv file
+        public string Textbook {  get; set; }
+        public string Subject { get; set; }
+        public float PurchasePrice { get; set; }
+        public string SalePrice { get; set; }
+        public string Rating { get; set; }
+
+    }
+}
